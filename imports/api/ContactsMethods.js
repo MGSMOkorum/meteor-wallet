4330c9e11975ae9a7b9930cf8e52c00cb5095700
Maria Gabriela Sándigo <mariagabrielasandigo@MacBook-Air-de-Maria.local>
2024-10-04 16:51:27 -0500

    remove method added

--- imports/api/ContactsMethods.js
+++ imports/api/ContactsMethods.js
@@ -1,13 +1,23 @@
 import { Meteor } from 'meteor/meteor';
+import { check } from 'meteor/check';
 import { ContactsCollection } from "./ContactsCollection"
 
 Meteor.methods({
-    'contacts.insert' ({ name, email, imageUrl }){
+    'contacts.insert'({ name, email, imageUrl }) {
 
-        if(!name){
+        check(name, String);
+        check(email, String);
+        check(imageUrl, String);
+
+        if (!name) {
             throw new Meteor.Error("Name is required")
         }
-        return ContactsCollection.insertAsync({ name, email, imageUrl, createdAt:new Date() });
+        return ContactsCollection.insertAsync({ name, email, imageUrl, createdAt: new Date() });
+    },
+
+    'contacts.remove'({ contactId }) {
+        check(contactId ,String)
+        return ContactsCollection.removeAsync(contactId);
     }
 })
 
