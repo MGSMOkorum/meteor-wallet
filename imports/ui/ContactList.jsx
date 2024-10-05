b0fcd7685553ca0ef7bbdbdd5dc2e5275aab4eb0
Maria Gabriela Sándigo <mariagabrielasandigo@MacBook-Air-de-Maria.local>
2024-10-04 22:23:47 -0500

    Publish and refactor on ContactList

--- imports/ui/ContactList.jsx
+++ imports/ui/ContactList.jsx
@@ -1,19 +1,55 @@
-import React from "react";
+import React, {memo} from "react";
 import { Meteor } from 'meteor/meteor';
 import { ContactsCollection } from "../api/ContactsCollection";
-import { useTracker, useSubscribe } from 'meteor/react-meteor-data'
+import { useSubscribe, useFind } from 'meteor/react-meteor-data'
 
 export const ContactList = () => {
-    useSubscribe("contacts");
 
-    const contacts = useTracker(() => {
-        return ContactsCollection.find({}, {sort:{cretedAt:-1}}).fetch();
-    });
+    const isLoading = useSubscribe("allContacts");
+    const contacts = useFind(() => ContactsCollection.find({}, {sort:{cretedAt:-1}}))
 
     const removeContact = (e,_id)=>{
         e.preventDefault()
-        Meteor.call('contacts.remove', {contactId:_id})
+        Meteor.callAsync('contacts.remove', {contactId:_id})
     }
+
+    if(isLoading()){
+        return (
+            <div>
+              <div className="mt-10">
+                <h3 className="text-xs font-semibold text-gray-500 uppercase tracking-wide">
+                L o a d i n g ...
+                </h3>
+              </div>
+            </div>
+          )
+    }
+    
+    // @ts-ignore
+    const ContactItem =memo(({contact}) => {
+        return (
+          <li className="py-4 flex items-center justify-between space-x-3">
+            <div className="min-w-0 flex-1 flex items-center space-x-3">
+              <div className="flex-shrink-0">
+                <img className="h-10 w-10 rounded-full" src={contact.imageUrl} alt="" />
+              </div>
+              <div className="min-w-0 flex-1">
+                <p className="text-sm font-medium text-gray-900 truncate">{contact.name}</p>
+                <p className="text-sm font-medium text-gray-500 truncate">{contact.email}</p>
+              </div>
+              <div>
+                <a
+                  href="#"
+                  onClick={(event) => removeContact(event, contact._id)}
+                  className="inline-flex items-center shadow-sm px-2.5 py-0.5 border border-gray-300 text-sm leading-5 font-medium rounded-full text-gray-700 bg-white hover:bg-gray-50"
+                >
+                  Remove
+                </a>
+              </div>
+            </div>
+          </li>
+        )
+      })
 
     return (
         <div>
@@ -22,29 +58,9 @@ export const ContactList = () => {
                     Contact List
                 </h3>
                 <ul role="list" className="mt-4 border-t border-b border-gray-200 divide-y divide-gray-200">
-                    {contacts.map((person, personIdx) => (
-                        <li key={personIdx} className="py-4 flex items-center justify-between space-x-3">
-                            <div className="min-w-0 flex-1 flex items-center space-x-3">
-                                <div className="flex-shrink-0">
-                                    <img className="h-10 w-10 rounded-full" src={person.imageUrl} alt="" />
-                                </div>
-                                <div className="min-w-0 flex-1">
-                                    <p className="text-sm font-medium text-gray-900 truncate">{person.name}</p>
-                                    <p className="text-sm font-medium text-gray-500 truncate">{person.email}</p>
-                                </div>
-                                <div>
-                                    <a
-                                    href="#"
-                                    onClick={(e)=> removeContact(e,person._id)}
-                                    className="inline-flex items-center shadow-sm px-2.5 py-0.5 border border-gray-300 text-sm leading-5 font-medium rounded-full text-gray-700 bg-white hover:bg-gray-50"
-                                    >
-                                    Remove
-                                    </a>
-                                </div>
-
-
-                            </div>
-                        </li>
+                    {contacts.map((contact) => (
+                        // @ts-ignore
+                        <ContactItem key={contact._id} contact={contact}/>
                     ))}
                 </ul>
             </div>
